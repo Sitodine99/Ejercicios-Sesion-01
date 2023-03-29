2f60e44de5e30995d043b1ebb067b8ae0da003dd
Jose Antonio Montero <josamontero@gmail.com>
2023-03-29 23:04:26 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EJERCICIO_1_y_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 1:");
+            Console.WriteLine("Escribe tu nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Tu nombre es " + nombre);
+            Console.WriteLine("Escribe la hora: ");
+            int hora = int.Parse(Console.ReadLine());
+            Console.WriteLine("Son las " + hora);
+            if (hora <= 12)
+            {
+                Console.WriteLine("Aún es mañanica");
+            } else
+            {
+                Console.WriteLine("Es ya tardecico");
+            }
+        }
+    }
+}
